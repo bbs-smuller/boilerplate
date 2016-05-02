cd4c5fe62fe701caa79f0ca66d7546a81a941bed
Sébastien Muller <smuller@big-boss-studio.com>
2016-05-02 09:33:48 +0200

    Start dev bottom bar FX

--- resources/assets/js/backend.js
+++ resources/assets/js/backend.js
@@ -10,3 +10,16 @@ new Vue({
 		LoginForm
 	}
 });
+
+// ------------------------------------------------------------------------------------
+
+setInterval(function () {
+	for (var i = 1 ; i <= 4 ; i++) {
+		var $color = document.querySelector('.color-' + i);
+		$color.classList.remove('color-' + i);
+
+		var next = i + 1;
+		next = (next === 5) ? 1 : next;
+		$color.classList.add('color-' + next);
+	}
+}, 5000);
