fd008f8ee18e4c91b502f3c4ae850f1e35aec7a5
Sébastien Muller <smuller@big-boss-studio.com>
2016-05-02 23:40:08 +0200

    Refactor ugly BottomBar

--- resources/assets/js/components/BottomBar.vue
+++ resources/assets/js/components/BottomBar.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="bar">
-		<div class="color color-{{ c1 }}"></div>
-		<div class="color color-{{ c2 }}"></div>
-		<div class="color color-{{ c3 }}"></div>
-		<div class="color color-{{ c4 }}"></div>
+		<div class="color color-{{ colors[0] }}"></div>
+		<div class="color color-{{ colors[1] }}"></div>
+		<div class="color color-{{ colors[2] }}"></div>
+		<div class="color color-{{ colors[3] }}"></div>
 	</div>
 </template>
 
@@ -14,10 +14,7 @@
 
 		data() {
 			return {
-				c1: 1,
-				c2: 2,
-				c3: 3,
-				c4: 4
+				colors: [1, 2, 3, 4]
 			};
 		},
 
@@ -29,30 +26,10 @@
 
 		methods: {
 			switchColors() {
-				this.c1 += 1;
-				if (this.c1 === 5) {
-					this.c1 = 1;
-				}
-
-				if (this.c1 === 1) {
-					this.c2 = 2;
-					this.c3 = 3;
-					this.c4 = 4;
-				} else {
-					if (this.c1 === 2) {
-						this.c2 = 3;
-						this.c3 = 4;
-						this.c4 = 1;
-					} else {
-						if (this.c1 === 3) {
-							this.c2 = 4;
-							this.c3 = 1;
-							this.c4 = 2;
-						} else {
-							this.c2 = 1;
-							this.c3 = 2;
-							this.c4 = 3;
-						}
+				for (var i = 0 ; i < 4 ; i++) {
+					this.colors[i] += 1;
+					if (this.colors[i] === 5) {
+						this.colors[i] = 1;
 					}
 				}
 			}
